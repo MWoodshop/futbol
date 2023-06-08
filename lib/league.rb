41a25c3f5cf157d8b888925fc920bb5bc24a35cc
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-06-08 16:56:55 -0400

    feat: added initialize

--- lib/league.rb
+++ lib/league.rb
@@ -1,2 +1,20 @@
 class League
+  attr_reader :teams, :games
+
+  def initialize(teams, games)
+    @teams = teams
+    @games = games
+  end
+
+  def home_wins
+    games.count(&:home_win?)
+  end
+
+  def visitor_wins
+    games.count(&:visitor_win?)
+  end
+
+  def total_games
+    games.count
+  end
 end
