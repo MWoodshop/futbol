109832e8cac8f415f08c10b43877c478e21b4407
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-06-08 16:57:29 -0400

    feat: adds initialize

--- lib/team.rb
+++ lib/team.rb
@@ -1,2 +1,17 @@
 class Team
+  attr_reader :team_id,
+              :franchise_id,
+              :team_name,
+              :team_abbreviation,
+              :team_stadium_name,
+              :team_api_url
+
+  def initialize(attributes)
+    @team_id = attributes[:team_id]
+    @franchise_id = attributes[:franchiseId]
+    @team_name = attributes[:teamName]
+    @team_abbreviation = attributes[:abbreviation]
+    @team_stadium_name = attributes[:Stadium]
+    @team_api_url = attributes[:link]
+  end
 end
