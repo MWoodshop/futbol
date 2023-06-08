eebe9707f22430135600e3ef269aaefacf3b1add
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-06-07 22:43:39 -0400

    test: adds initial tests to game class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,14 +1,15 @@
 require 'spec_helper'
 
 RSpec.describe Game do
-  before :each do
-    file_name = './data/games.csv'
-    @reader = Game.new(file_name)
+  before do
+    allow(File).to receive(:read).and_return("game_id,season,date_time,away_team_id,home_team_id,away_goals,home_goals,venue,venue_link\n1,2012030221,2012-05-16,3,6,2,3,TD Garden,/api/v1/venues/null\n2,2012030222,2012-05-19,3,6,2,2,TD Garden,/api/v1/venues/null\n")
   end
 
   describe '#initialize' do
     it 'can initialize' do
-      expect(@reader.data).to include(['Value 1', 'Value 2', 'Value 3'])
+      game = Game.new
+
+      expect(game).to be_a(Game)
     end
   end
 end
