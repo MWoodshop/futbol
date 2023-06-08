751cb572ead6900a7eaec8dcf1773d3e46219f3b
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-06-08 16:57:22 -0400

    feat: adds percentage_home_wins and percentage_visitor_wins methods

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,15 +1,16 @@
 require 'csv'
 require_relative 'game'
 require_relative 'team'
-require_relative 'game_team'
+require_relative 'season'
+require_relative 'league'
 
 class StatTracker
-  attr_reader :games, :teams, :game_team
+  attr_reader :games, :teams, :seasons
 
   def initialize(locations)
     @games = load_games(locations[:games])
     @teams = load_teams(locations[:teams])
-    @game_teams = load_game_teams(locations[:game_teams])
+    @seasons = load_seasons(@games)
   end
 
   def self.from_csv(locations)
@@ -17,6 +18,11 @@ class StatTracker
   end
 
   def load_games(file_path)
+    games = []
+    CSV.foreach(file_path, headers: true, header_converters: :symbol) do |row|
+      games << Game.new(row)
+    end
+    games
     # Load game data from CSV file and store it as Game objects in an array
   end
 
@@ -24,9 +30,19 @@ class StatTracker
     # Load team data from CSV file and store it as Team objects in an array
   end
 
-  def load_game_teams(file_path)
-    # Load game team data from CSV file and store it as GameTeam objects in an array
+  def load_seasons(file_path)
+    # Group games by season and store it as Season objects in an array
   end
 
   # Add statistical methods here...
+
+  def percentage_home_wins
+    home_wins = @games.count(&:home_win?)
+    (home_wins.to_f / @games.size).round(2)
+  end
+
+  def percentage_visitor_wins
+    visitor_wins = @games.count(&:visitor_win?)
+    (visitor_wins.to_f / @games.size).round(2)
+  end
 end
