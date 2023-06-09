bfcd84c5ff8e3c98b2dfb366043e4733e18bf608
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-06-08 21:13:47 -0400

    feat: adds winningest_coach and worst_coach methods

--- lib/season.rb
+++ lib/season.rb
@@ -5,4 +5,36 @@ class Season
     @season_id = season_id
     @games = games
   end
+
+  def winningest_coach
+    coach_wins = Hash.new(0)
+
+    @games.each do |game|
+      if game.winner
+        winning_coach = game.winner.coach
+        coach_wins[winning_coach] += 1
+      end
+    end
+
+    coach_wins.max_by { |_coach, wins| wins }.first
+  end
+
+  def worst_coach
+    coach_wins = Hash.new(0)
+    coach_games = Hash.new(0)
+
+    @games.each do |game|
+      game.game_teams.each do |game_team|
+        coach_games[game_team.coach] += 1
+        coach_wins[game_team.coach] += 1 if game_team.win?
+      end
+    end
+
+    coach_win_percentages = {}
+    coach_games.each do |coach, games|
+      coach_win_percentages[coach] = coach_wins[coach].to_f / games * 100
+    end
+
+    coach_win_percentages.min_by { |_coach, win_percentage| win_percentage }.first
+  end
 end
