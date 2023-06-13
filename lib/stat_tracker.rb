3ef4de7d8328608b3a0741cf6e3b0edb2042a081
Adrian Roussev <a.roussev@mail.utoronto.ca>
2023-06-13 10:46:10 -0400

    edit with tests

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -3,59 +3,53 @@ require_relative 'game'
 require_relative 'team'
 require_relative 'game_team'
 
+require 'csv'
+
 class StatTracker
-  attr_reader :games, :teams, :game_teams, :seasons, :league
+  attr_reader :games, :teams, :game_teams
 
-  def self.from_csv(locations)
-    StatTracker.new(locations)
+  def initialize(locations)
+    @games = []
+    @teams = []
+    @game_teams = []
+
+    import_data(locations[:games], locations[:teams], locations[:game_teams])
   end
 
-  def initialize(data)
-    @games = game_init(data)
-    @teams = teams_init(data)
-    @game_teams = game_teams_init(data)
+  def import_data(games_path, teams_path, game_teams_path)
+    @games = CSV.read(games_path, headers: true, header_converters: :symbol).map do |row|
+      Game.new(row.to_h)
+    end
+
+    @teams = CSV.read(teams_path, headers: true, header_converters: :symbol).map do |row|
+      Team.new(row.to_h)
+    end
+
+    @game_teams = CSV.read(game_teams_path, headers: true, header_converters: :symbol).map do |row|
+      GameTeam.new(row.to_h)
+    end
   end
 
-  def game_init(data)
-    CSV.read(data[:games], headers: true, header_converters: :symbol).map { |row| Game.new(row) }
+  def total_goals_in_game
+    @games.map do |game|
+      game.away_goals + game.home_goals
+    end
   end
-
-  def teams_init(data)
-    CSV.read(data[:teams], headers: true, header_converters: :symbol).map { |row| Team.new(row) }
-  end
-
-  def game_teams_init(data)
-    CSV.read(data[:game_teams], headers: true, header_converters: :symbol).map { |row| GameTeam.new(row) }
-  end
-
-  # GAME STATS
-
- 
 
   def count_of_games_by_season
-    games_by_season = {}
+    games_by_season = Hash.new(0)
     @games.each do |game|
-    
-      if games_by_season.include?(game.season_id)
-        games_by_season[game.season_id] += 1 
-      else
-        games_by_season[game.season_id] = 1 
-      end
+      games_by_season[game.season_id] += 1
     end
     games_by_season
-   
   end
 
   def count_of_goals_by_season
-    goals_by_season = {}
+    goals_by_season = Hash.new(0)
     @games.each do |game|
-      if goals_by_season.include?(game.season_id)
-        goals_by_season[game.season_id] += ([game.away_goals.to_f]+[game.away_goals.to_f])
-      elsif !goals_by_season.include?(game.season_id)
-        goals_by_season[game.season_id] = ([game.away_goals.to_f]+[game.home_goals.to_f])
-      end 
-    goals_by_season
+      goals_by_season[game.season_id] += game.away_goals + game.home_goals
     end
+    goals_by_season
   end
 
   def average_goals_per_game
@@ -63,30 +57,18 @@ class StatTracker
   end
 
   def average_goals_by_season
-    average_goals = {}  
-    count_of_goals_by_season.each do|season, goals|
-      count_of_games_by_season.each do|season_id, games|
-        if season_id == season
-        average_goals[season_id] = (goals/games).round(2) 
-        end
-      end
+    average_goals = {}
+    count_of_goals_by_season.each do |season, goals|
+      average_goals[season] = (goals.to_f / count_of_games_by_season[season]).round(2)
     end
     average_goals
   end
 
-
-
-  # LEAGUE STATS
-
- 
-
   def team_goals(home_or_away)
-    team_goals = {}
+    team_goals = Hash.new { |hash, key| hash[key] = [] }
     @game_teams.each do |game_team|
-      if team_goals[game_team.team_id] != nil && game_team.home_or_away == home_or_away
-        team_goals[game_team.team_id].push(game_team.goals.to_i)
-      elsif  game_team.home_or_away == home_or_away
-        team_goals[game_team.team_id] = [game_team.goals.to_i]
+      if game_team.home_or_away == home_or_away
+        team_goals[game_team.team_id] << game_team.goals.to_i
       end
     end
     team_goals
@@ -101,80 +83,211 @@ class StatTracker
   end
 
   def team_goal_average(home_or_away)
-      team_goals = team_goals(home_or_away)
-      average_goals(team_goals)
-    end
+    team_goals = team_goals(home_or_away)
+    average_goals(team_goals)
   end
 
   def find_team_name(team_element)
-    @teams.each do |team|
-      if team_element[0] == team.team_id
-        team_name = team.team_name
-      end
-    end
+    team = @teams.find { |team| team.team_id == team_element[0] }
+    team&.team_name
   end
+  
 
   def highest_scoring_visitor_team
-    highest_scoring = team_goal_average('away').max_by{ |team_id, average_goals| average_goals }
+    highest_scoring = team_goal_average('away').max_by { |team_id, average_goals| average_goals }
     find_team_name(highest_scoring)
   end
 
-
   def lowest_scoring_visitor_team
-    lowest_scoring = team_goal_average('away').min_by{ |id, average| average }
-  find_team_name(lowest_scoring)
+    lowest_scoring = team_goal_average('away').min_by { |team_id, average_goals| average_goals }
+    find_team_name(lowest_scoring)
   end
 
-  # SEASON STATS
- 
-
-
+  
+  # def season_data(all_seasons)
+  #   @all_seasons = @games.map { |game| game.season_id }.uniq
+  #   @season_data = {}
+  
+  #   @all_seasons.each do |season_id|
+  #     season = @game_teams.select { |game_team| game_team.game_id[0, 4] == season_id[0, 4] }
+  #     @season_data[season_id[0, 4]] = season
+  #   end
+  
+  #   @season_data
+  # end
 
   def season_data(all_seasons)
-    all_seasons = @games.map { |game| game[:season] }.uniq
-    @season_data = {}
-    season = []
-    @game_teams.select do |game_team|      
-      all_seasons.select do |season_id|
-        if season_id[0,4] == game_team.game_id[0,4]
-          season << game_team
-          season_data[season_id[0,4]] = season
+        all_seasons = @games.map { |game| game.season_id }.uniq
+        @season_data = {}
+        season = []
+        @game_teams.select do |game_team|
+          all_seasons.select do |season_id|
+            if season_id[0,4] == game_team.game_id[0,4]
+              season << game_team
+              season_data[season_id[0,4]] = season
+            end
+          end
         end
+        @season_data
       end
-    end
-    @season_data
-  end
-
+  
   def team_accuracy(season)
-    season_goals = {}
-    team_shots = {}
-
-    game_team_season = @season_data[season]
+    season_data= season_data(@all_seasons) 
+    season = season_data[season]
+    
+  
+    season_goals = Hash.new(0)
+    team_shots = Hash.new(0)
+  
+    season_data.each do |game_team_season|
       season_goals[game_team_season.team_id] += game_team_season.goals.to_i
       team_shots[game_team_season.team_id] += game_team_season.shots.to_i
-    
+    end
+  
     team_accuracy = {}
-
-    team_accuracy.update(season_goals,team_shots) do |team,goals,shots|
-      goals.fdiv(shots).round(4)*100
-   
+    season_goals.each do |team_id, goals|
+      shots = team_shots[team_id]
+      if shots.zero?
+        team_accuracy[team_id] = 0.0
+      else
+        accuracy = (goals.fdiv(shots) * 100).round(4)
+        team_accuracy[team_id] = accuracy
+      end
+    end
+  
     team_accuracy
   end
 
-
   def most_accurate_team(season)
-    find_team_name(team_accuracy(season).max_by { |team_id,percent_accuracy| percent_accuracy })
-  end
+        find_team_name(team_accuracy(season).max_by { |team_id,percent_accuracy| percent_accuracy })
+      end
+      def least_accurate_team(season)
+        find_team_name(team_accuracy(season).min_by { |team_id,percent_accuracy| percent_accuracy })
+      end
+    end
+  
+#   def most_accurate_team(season)
+#     team_accuracy = team_accuracy(season)
+  
+#     team_id = team_accuracy.max_by { |_, percent_accuracy| percent_accuracy }.first
+#     find_team_name(team_id)
+#   end
+  
+#   def least_accurate_team(season)
+#     team_accuracy = team_accuracy(season)
+  
+#     team_id = team_accuracy.min_by { |_, percent_accuracy| percent_accuracy }.first
+#     find_team_name(team_id)
+#   end
+# end 
 
 
-  def least_accurate_team(season)
-    find_team_name(team_accuracy(season).min_by { |team_id,percent_accuracy| percent_accuracy })
-  end
-end
-
-
-
-
+# ef count_of_games_by_season
+#     games_by_season = {}
+#     @games.each do |game|
+#       if games_by_season.include?(game.season_id)
+#         games_by_season[game.season_id] += 1
+#       else
+#         games_by_season[game.season_id] = 1
+#       end
+#     end
+#     games_by_season
+#   end
+#   def count_of_goals_by_season
+#     goals_by_season = {}
+#     @games.each do |game|
+#       if goals_by_season.include?(game.season_id)
+#         goals_by_season[game.season_id] += ([game.away_goals.to_f]+[game.away_goals.to_f])
+#       elsif !goals_by_season.include?(game.season_id)
+#         goals_by_season[game.season_id] = ([game.away_goals.to_f]+[game.home_goals.to_f])
+#       end
+#     goals_by_season
+#     end
+#   end
+#   def average_goals_per_game
+#     (total_goals_in_game.sum / @games.count.to_f).round(2)
+#   end
+#   def average_goals_by_season
+#     average_goals = {}
+#     count_of_goals_by_season.each do|season, goals|
+#       count_of_games_by_season.each do|season_id, games|
+#         if season_id == season
+#         average_goals[season_id] = (goals/games).round(2)
+#         end
+#       end
+#     end
+#     average_goals
+#   end
+#   def team_goals(home_or_away)
+#     team_goals = {}
+#     @game_teams.each do |game_team|
+#       if team_goals[game_team.team_id] != nil && game_team.home_or_away == home_or_away
+#         team_goals[game_team.team_id].push(game_team.goals.to_i)
+#       elsif  game_team.home_or_away == home_or_away
+#         team_goals[game_team.team_id] = [game_team.goals.to_i]
+#       end
+#     end
+#     team_goals
+#   end
+#   def average_goals(team_goals)
+#     team_average = {}
+#     team_goals.each do |team_id, goals_per_game|
+#       team_average[team_id] = (goals_per_game.sum.to_f / goals_per_game.size.to_f).round(3)
+#     end
+#     team_average
+#   end
+#   def team_goal_average(home_or_away)
+#       team_goals = team_goals(home_or_away)
+#       average_goals(team_goals)
+#     end
+#   end
+#   def find_team_name(team_element)
+#     @teams.each do |team|
+#       if team_element[0] == team.team_id
+#         team_name = team.team_name
+#       end
+#     end
+#   end
+#   def highest_scoring_visitor_team
+#     highest_scoring = team_goal_average('away').max_by{ |team_id, average_goals| average_goals }
+#     find_team_name(highest_scoring)
+#   end
+#   def lowest_scoring_visitor_team
+#     lowest_scoring = team_goal_average('away').min_by{ |id, average| average }
+#   find_team_name(lowest_scoring)
+#   end
+#   # SEASON STATS
+#   def season_data(all_seasons)
+#     all_seasons = @games.map { |game| game[:season] }.uniq
+#     @season_data = {}
+#     season = []
+#     @game_teams.select do |game_team|
+#       all_seasons.select do |season_id|
+#         if season_id[0,4] == game_team.game_id[0,4]
+#           season << game_team
+#           season_data[season_id[0,4]] = season
+#         end
+#       end
+#     end
+#     @season_data
+#   end
+#   def team_accuracy(season)
+#     season_goals = {}
+#     team_shots = {}
+#     game_team_season = @season_data[season]
+#       season_goals[game_team_season.team_id] += game_team_season.goals.to_i
+#       team_shots[game_team_season.team_id] += game_team_season.shots.to_i
+#     team_accuracy = {}
+#     team_accuracy.update(season_goals,team_shots) do |team,goals,shots|
+#       goals.fdiv(shots).round(4)*100
+#     team_accuracy
+#   end
+#   def most_accurate_team(season)
+#     find_team_name(team_accuracy(season).max_by { |team_id,percent_accuracy| percent_accuracy })
+#   end
+#   def least_accurate_team(season)
+#     find_team_name(team_accuracy(season).min_by { |team_id,percent_accuracy| percent_accuracy })
+#   end
 
 
 
--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -1,51 +1,136 @@
-# require '../futbol/lib/stat_tracker'
-# require 'rspec/mocks/standalone'
+require 'rspec'
+require 'csv'
+require './lib/game.rb'
+require './lib/team.rb'
+require './lib/game_team.rb'
+require './lib/stat_tracker.rb'
 
-# RSpec.describe StatTracker do
-#   before(:each) do
-#     game_double = double('Game')
-#     team_double = double('Team')
-#     game_team_double = double('GameTeam')
+RSpec.describe StatTracker do
+  let(:games_path) { './data/games.csv' }
+  let(:teams_path) { '/data/teams.csv' }
+  let(:game_teams_path) { './data/game_teams.csv' }
 
-#     # Set up stubbed return values for the game double's methods
-#     allow(game_double).to receive(:game_id).and_return('1')
-#     allow(game_double).to receive(:season_id).and_return('374')
-#     allow(game_double).to receive(:season_type).and_return('postseason')
-#     allow(game_double).to receive(:game_date).and_return('5/16/13')
-#     allow(game_double).to receive(:away_team_id).and_return(5)
-#     allow(game_double).to receive(:home_team_id).and_return(6)
-#     allow(game_double).to receive(:away_goals).and_return(2)
-#     allow(game_double).to receive(:home_goals).and_return(3)
-#     allow(game_double).to receive(:venue_name).and_return('Toyota Stadium')
-#     allow(game_double).to receive(:venue_link).and_return('/api/v1/venues/null')
+  let(:games_data) do
+    [
+      { game_id: '1', season: '20182019', type: 'Regular Season', date_time: '2018-10-03',
+        away_team_id: '3', home_team_id: '6', away_goals: '2', home_goals: '5',
+        venue: 'Arena 1', venue_link: 'arena1.com' },
+      { game_id: '2', season: '20182019', type: 'Regular Season', date_time: '2018-10-04',
+        away_team_id: '4', home_team_id: '6', away_goals: '3', home_goals: '4',
+        venue: 'Arena 2', venue_link: 'arena2.com' }
+    ]
+  end
 
-#     @games = [game_double]
-#     @teams = [team_double]
-#     @game_teams = [game_team_double]
+  let(:teams_data) do
+    [
+      { team_id: '3', franchiseid: '10', teamname: 'Team 1', abbreviation: 'T1',
+        stadium: 'Stadium 1', link: 'team1.com' },
+      { team_id: '4', franchiseid: '11', teamname: 'Team 2', abbreviation: 'T2',
+        stadium: 'Stadium 2', link: 'team2.com' },
+      { team_id: '6', franchiseid: '13', teamname: 'Team 3', abbreviation: 'T3',
+        stadium: 'Stadium 3', link: 'team3.com' }
+    ]
+  end
 
-#     allow_any_instance_of(StatTracker).to receive(:game_init).and_return(@games)
-#     allow_any_instance_of(StatTracker).to receive(:teams_init).and_return(@teams)
-#     allow_any_instance_of(StatTracker).to receive(:game_teams_init).and_return(@game_teams)
-#   end
+  let(:game_teams_data) do
+    [
+      { game_id: '1', team_id: '3', hoa: 'away', result: 'loss', settled_in: 'regulation',
+        head_coach: 'Coach 1', goals: '2', shots: '30', tackles: '10', pim: '4',
+        powerplayopportunities: '4', powerplaygoals: '1', faceoffwinpercentage: '50.0',
+        giveaways: '5', takeaways: '2' },
+      { game_id: '1', team_id: '6', hoa: 'home', result: 'win', settled_in: 'regulation',
+        head_coach: 'Coach 2', goals: '5', shots: '40', tackles: '12', pim: '6',
+        powerplayopportunities: '3', powerplaygoals: '1', faceoffwinpercentage: '55.0',
+        giveaways: '3', takeaways: '4' },
+      { game_id: '2', team_id: '4', hoa: 'away', result: 'win', settled_in: 'regulation',
+        head_coach: 'Coach 3', goals: '3', shots: '35', tackles: '8', pim: '3',
+        powerplayopportunities: '5', powerplaygoals: '2', faceoffwinpercentage: '52.5',
+        giveaways: '4', takeaways: '3' },
+      { game_id: '2', team_id: '6', hoa: 'home', result: 'loss', settled_in: 'regulation',
+        head_coach: 'Coach 2', goals: '4', shots: '38', tackles: '10', pim: '5',
+        powerplayopportunities: '2', powerplaygoals: '1', faceoffwinpercentage: '51.0',
+        giveaways: '2', takeaways: '5' }
+    ]
+  end
 
-#   # Use a `let` statement to create the StatTracker object.
-#   let(:stat_tracker) { StatTracker.new({}) }
+  let(:locations) do
+    {
+      games: games_path,
+      teams: teams_path,
+      game_teams: game_teams_path
+    }
+  end
 
-#   describe '#home_win?' do
-#     it 'returns true when the home team has more goals' do
-#       expect(stat_tracker.home_win?).to be true
-#     end
-#   end
+  before do
+    allow(CSV).to receive(:read).with(games_path, headers: true, header_converters: :symbol)
+                                 .and_return(games_data)
+    allow(CSV).to receive(:read).with(teams_path, headers: true, header_converters: :symbol)
+                                 .and_return(teams_data)
+    allow(CSV).to receive(:read).with(game_teams_path, headers: true, header_converters: :symbol)
+                                 .and_return(game_teams_data)
+  end
 
-#   describe '#away_win?' do
-#     it 'returns false when the away team has fewer goals' do
-#       expect(stat_tracker.away_win?).to be false
-#     end
-#   end
+  subject(:stat_tracker) { described_class.new(locations) }
 
-#   describe '#game_tie?' do
-#     it 'returns false when the home team and away team have a different number of goals' do
-#       expect(stat_tracker.game_tie?).to be false
-#     end
-#   end
-# end
+  describe 'initialize' do
+    it 'imports games, teams, and game_teams data' do
+      expect(stat_tracker.games.size).to eq(2)
+      expect(stat_tracker.teams.size).to eq(3)
+      expect(stat_tracker.game_teams.size).to eq(4)
+    end
+  end
+
+  describe 'total_goals_in_game' do
+    it 'returns an array of total goals in each game' do
+      expect(stat_tracker.total_goals_in_game).to eq([7, 7])
+    end
+  end
+
+  describe 'count_of_games_by_season' do
+    it 'returns a hash with the count of games by season' do
+      expect(stat_tracker.count_of_games_by_season).to eq({ '20182019' => 2 })
+    end
+  end
+
+  describe 'count_of_goals_by_season' do
+    it 'returns a hash with the count of goals by season' do
+      expect(stat_tracker.count_of_goals_by_season).to eq({ '20182019' => 14 })
+    end
+  end
+
+  describe 'average_goals_per_game' do
+    it 'returns the average goals per game' do
+      expect(stat_tracker.average_goals_per_game).to eq(7.0)
+    end
+  end
+
+  describe 'average_goals_by_season' do
+    it 'returns a hash with the average goals per game by season' do
+      expect(stat_tracker.average_goals_by_season).to eq({ '20182019' => 7.0 })
+    end
+  end
+
+  describe 'highest_scoring_visitor_team' do
+    it 'returns the name of the highest scoring visitor team' do
+      expect(stat_tracker.highest_scoring_visitor_team).to eq('Team 2')
+    end
+  end
+
+  describe 'lowest_scoring_visitor_team' do
+    it 'returns the name of the lowest scoring visitor team' do
+      expect(stat_tracker.lowest_scoring_visitor_team).to eq('Team 1')
+    end
+  end
+
+  describe 'most_accurate_team' do
+    it 'returns the name of the most accurate team in a given season' do
+      expect(stat_tracker.most_accurate_team('20182019')).to eq('Team 3')
+    end
+  end
+
+  describe 'least_accurate_team' do
+    it 'returns the name of the least accurate team in a given season' do
+      expect(stat_tracker.least_accurate_team('20182019')).to eq('Team 1')
+    end
+  end
+end
